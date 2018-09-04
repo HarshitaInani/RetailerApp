378e49991c6c376fb299b899b15ffeb28c2c64d9
HarshitaInani <harshita@moreyeahs.in>
2018-09-04 15:31:46 +0530

    add filter

---
+++ app/src/main/java/app/retailer/krina/shop/com/mp_shopkrina_retailer/TradeofferActivity.java
@@ -0,0 +1,13 @@
+package app.retailer.krina.shop.com.mp_shopkrina_retailer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TradeofferActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tradeoffer2);
+    }
+}
